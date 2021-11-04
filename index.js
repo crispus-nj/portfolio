45680f4d8611841e2757829652e40f7fda2cc375
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-04 16:21:13 +0300

    added index.js to the index.html. selected the contact me button displayed successfully to the console

---
+++ index.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('.btn-contact')
+console.dir(button)
+// for( let i = 0; i<button.length; i++){
+//     let buttonClicked = button[i]
+//     console.dir("Contact me clicked successfully", buttonClicked)
+// }
