fc625edbe76a59a033e11e0faff998969253fd3a
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-05 16:56:15 +0300

    removed the hover effect on the navbar automatically use the bootstrap defaults.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let button = document.querySelector('.btn-contact')
-console.dir(button)
+// console.dir(button)
 // for( let i = 0; i<button.length; i++){
 //     let buttonClicked = button[i]
 //     console.dir("Contact me clicked successfully", buttonClicked)
 // }
+
